071fc8bea40ed15b52d7a5d799a7b1d732d0923a
Kimseokju <ruhyun92@gmail.com>
2024-03-12 08:58:34 +0900

    리시버 추가

--- app/user/models.py
+++ app/user/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from customshop.utils.timestamp import TimeStampedModel
 
@@ -69,6 +71,17 @@ class UserProfile(TimeStampedModel):
 
     def __str__(self) -> str:
         return f"{self.user.username}'s Profile"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class UserAddress(TimeStampedModel):
